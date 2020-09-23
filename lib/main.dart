f450f6a4a2d0fd3c9a1c9f0b2cdabf959511b591
HuyN1201 <huyn120198@gmail.com>
2020-09-22 21:49:05 -0500

    Changes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-          title: Center(child: Text('I am Poor')),
-        ),
-        body: Center(
-            child: Image(
-              image: AssetImage('images/icons8-opened-folder-512.png'),
-            )
-        ),
-        backgroundColor: Colors.black54,
-      )
-  ));
+    appBar: AppBar(
+      backgroundColor: Colors.amberAccent,
+      title: Center(child: Text('Testing commits')),
+    ),
+    body: Center(
+        child: Image(
+      image: AssetImage('images/icons8-opened-folder-512.png'),
+    )),
+    backgroundColor: Colors.black54,
+  )));
 }
-
