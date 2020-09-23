d4e2d61a83b0a58030394f309fe95c2844c431f0
Huy Nguyen <huyn120198@gmail.com>
2020-09-22 20:51:59 -0500

    Initial commits

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Center(child: Text('I am Poor')),
+      ),
+      body: Center(
+        child: Image(
+            image: AssetImage('images/icons8-opened-folder-512.png'),
+        )
+      ),
+      backgroundColor: Colors.black54,
+    )
+  ));
+}
+
