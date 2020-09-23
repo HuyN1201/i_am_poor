dd4163075b22ab0e23177c57b3262622c7f83182
Huy Nguyen <huyn120198@gmail.com>
2020-09-22 21:40:04 -0500

    Changes from PC

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.black12,
-      title: Center(child: Text('Bertos a hoe ass bitch')),
-    ),
-    body: Center(
-        child: Image(
-      image: AssetImage('images/icons8-opened-folder-512.png'),
-    )),
-    backgroundColor: Colors.black54,
-  )));
+        appBar: AppBar(
+          backgroundColor: Colors.amberAccent,
+          title: Center(child: Text('I am Poor')),
+        ),
+        body: Center(
+            child: Image(
+              image: AssetImage('images/icons8-opened-folder-512.png'),
+            )
+        ),
+        backgroundColor: Colors.black54,
+      )
+  ));
 }
+
